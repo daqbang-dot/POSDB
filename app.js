4d5afa003658d014afda39058e0aed80a8f17e22
daqbang-dot <daqbang@gmail.com>
2026-01-28 00:59:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -88,3 +88,10 @@ function convertToInvoice(quoteId) {
     saveData();
     alert(`Success! ${quoteId} has been converted to ${newInvoice.id}`);
 }
+function markAsPaid(docId) {
+    const doc = documents.find(d => d.id === docId);
+    if (doc) {
+        doc.status = 'Paid';
+        saveData();
+    }
+}
